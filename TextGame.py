89532febe74db4980ed0d7985f3d4a285865297e
Shah Alom <94467224+ShahsTestingZone@users.noreply.github.com>
2023-09-18 15:52:32 +0100

    Shah added his stuff

--- TextGame.py
+++ TextGame.py
@@ -49,3 +49,49 @@ def get_action(room):
             return room['west']
         else:
             print('Invalid action. Try again.')
+
+# Shah's Work
+
+room1 = "Living Room"
+room2 = "Bedroom"
+room3 = "Kitchen"
+# add 3 other rooms
+
+#
+distanceWalked = 0 # counts how many moves they have made or walked
+movesAllowed = 10
+currentRoom = ""
+#Room and Description
+
+rooms = {room1: "Description 1", room2: "Description 2", room3: "Description 3"}
+
+print("you have entered a haunted house and walked through the front door you see a zombie and you need to head to the kitchen to get a knife")
+print("you are in the hallway entrance, please move towards the living room to then access the kitchen")
+
+
+move = input("What room would you like to go in:")
+
+
+
+#switch statement 
+if age > 90:
+    print("You are too old to party, granny.")
+elif age < 0:
+    print("You're yet to be born")
+elif age >= 18:
+    print("You are allowed to party")
+else: 
+    "You're too young to party"
+
+# Output: You are too old to party, granny.
+
+def roomDesc(currentRoom):
+    print (rooms[currentRoom])
+
+def roomsEntered(distanceWalked):
+    print("You have entered {rdistanceWalked} and have x amount of life remaininng")
+
+def updateVariables(room):
+    distanceWalked =+ 1
+    currentRoom = room
+
